466aac4fafb11e416753501cca7c8258652dc3da
yogithaaratla <93836663+yogithaaratla@users.noreply.github.com>
2022-12-27 22:54:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", { allTodos });
+  } else {
+    response.json({ allTodos });
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
